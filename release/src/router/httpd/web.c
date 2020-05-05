588d5f1bfd9133c00df3568b43549bf1166a5adb
Rokis <ghost1988102@gmail.com>
2020-05-06 00:46:35 +0800

    upgrade: seprate cfe from image(useless for now).
    Signed-off-by: Rokis <ghost1988102@gmail.com>

--- release/src/router/httpd/web.c
+++ release/src/router/httpd/web.c
@@ -11632,6 +11632,7 @@ do_upgrade_post(char *url, FILE *stream, int len, char *boundary)
 	char *reset = safe_get_cgi_json("reset",NULL);
 
 	char upload_fifo[64] = "/tmp/linux.trx";
+	char *upload_fifo_cfe = "/tmp/cfe.bin";
 	FILE *fifo = NULL;
 	char buf[4096];
 	int ch/*, ver_chk = 0*/;
@@ -11727,6 +11728,8 @@ do_upgrade_post(char *url, FILE *stream, int len, char *boundary)
 	filelen = len;
 	cnt = 0;
 	offset = 0;
+	int ret_chk = 0, left_wr = 0;
+	const int size_cfe = 512*1024; //512kb
 
 	/* Pipe the rest to the FIFO */
 	while (len>0 && filelen>0)
@@ -11750,7 +11753,7 @@ do_upgrade_post(char *url, FILE *stream, int len, char *boundary)
 		}
 #endif
 
-		count = fread(buf + offset, 1, MIN(len, sizeof(buf)-offset), stream);
+		count = fread(buf + offset, 1, MIN(MIN(len, sizeof(buf)-offset), filelen), stream);
 
 		if(count <= 0)
 			goto err;
@@ -11774,12 +11777,32 @@ do_upgrade_post(char *url, FILE *stream, int len, char *boundary)
 			_dprintf("read from stream: %d\n", count);
 			cnt++;
 
-			if(!check_imageheader(buf, &filelen)) {
+			ret_chk = check_imageheader(buf, &filelen);
+			if(!ret_chk) {
 				goto err;
+			} else if(ret_chk == 2) {
+				if (fifo)
+					fclose(fifo);
+				if (!(fifo = fopen(upload_fifo_cfe, "w"))) goto err;
+				left_wr = size_cfe;
 			}
 		}
 		filelen-=count;
-		fwrite(buf, 1, count, fifo);
+		if(ret_chk == 2) {
+			left_wr -= count;
+			if(left_wr >= 0)
+				fwrite(buf, 1, count, fifo);
+			if(filelen <= 0) {
+				fclose(fifo);
+				if (!(fifo = fopen(upload_fifo, "w"))) goto err;
+				filelen = len;
+				cnt = 0;
+			}
+		}
+		else if(ret_chk == 1)
+		{
+			fwrite(buf, 1, count, fifo);
+		}
 	}
 
 #ifdef HND_ROUTER
@@ -11892,6 +11915,7 @@ do_upgrade_cgi(char *url, FILE *stream)
 			nvram_set_int("upgrade_fw_status", FW_TRX_CHECK_ERROR);
 		else	/* 1: illegal image */
 			nvram_set_int("upgrade_fw_status", FW_WRITING_ERROR);
+		unlink("/tmp/cfe.bin");
 		unlink("/tmp/linux.trx");
 
 		if (stop_upgrade_once) {
