3c7a6299ae33c3426bcfd881768755cf40c7e3d0
Rokis <ghost1988102@gmail.com>
2020-05-08 19:58:51 +0800

    ubifs: fix JFFS2 option
    Signed-off-by: Rokis <ghost1988102@gmail.com>

--- release/src/router/rc/ubifs.c
+++ release/src/router/rc/ubifs.c
@@ -121,6 +121,11 @@ static inline int ubifs_erase(int dev, int part)
 
 void start_ubifs(void)
 {
+	if (!nvram_match("jffs2_enable", "1")) {
+		notice_set("jffs", "");
+		return;
+	}
+
 	int format = 0;
 	char s[256];
 	int dev, part, size;
@@ -213,8 +218,9 @@ void start_ubifs(void)
 
 	_dprintf("*** ubifs: %s %d, %d, %d\n", UBIFS_VOL_NAME, dev, part, size);
 #ifndef RTCONFIG_NVRAM_FILE
-	if (nvram_match("ubifs_format", "1")) {
+	if (nvram_match("ubifs_format", "1") || nvram_match("jffs2_format", "1")) {
 		nvram_set("ubifs_format", "0");
+		nvram_set("jffs2_format", "0");
 
 		if (ubifs_erase(dev, part)) {
 			error("formatting");
--- release/src/router/shared/defaults.c
+++ release/src/router/shared/defaults.c
@@ -3174,7 +3174,7 @@ struct nvram_tuple router_defaults[] = {
 	{ "nfsd_exportlist", "", CKN_STR2048, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },
 #endif
 
-#if defined(RTCONFIG_JFFS2) || defined(RTCONFIG_BRCM_NAND_JFFS2)
+#if defined(RTCONFIG_JFFS2) || defined(RTCONFIG_BRCM_NAND_JFFS2) || defined(RTCONFIG_UBIFS)
 	{ "jffs2_enable", "1", CKN_STR1, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },
 	{ "jffs2_scripts", "0", CKN_STR1, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },
 	{ "jffs2_format", "0", CKN_STR1, CKN_TYPE_DEFAULT, CKN_ACC_LEVEL_DEFAULT, CKN_ENC_DEFAULT, 0 },
